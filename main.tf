0bb599163be63d08d36fc27d0bfd91e0d5c13a9b
JChen-AC <chen1101@algonquinlive.com>
2026-06-14 22:03:28 -0400

    applying nsg to subnet

--- main.tf
+++ main.tf
@@ -110,6 +110,12 @@ terraform {
 
     }
   }
+
+  resource "azurerm_subnet_network_security_group_association" "example"{
+    subnet_id = azurerm.subnet.example.id
+    network_security_group_id = azurerm_network_security_group.example.id    
+  }
+  
     
 }
 
