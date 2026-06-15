121a54de3e33b898ffc73686adf27b4dda9cef2b
JChen-AC <chen1101@algonquinlive.com>
2026-06-14 22:01:50 -0400

    Adding nic

--- main.tf
+++ main.tf
@@ -95,6 +95,21 @@ terraform {
       destination_address_prefix = "*"
     }
   }
+
+  resource "azurerm_network_interface" "example" {
+    name = "${var.labelPrefix}nsg"
+    location = "${var.region}"
+    resource_group_name = azurerm_resource_group.main.name   
+
+    ip_configuration{
+      name = "vmnic"
+      subnet_id = azurerm_subnet.example.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id = azurerm_public_ip.example.id
+
+
+    }
+  }
     
 }
 
